0418ac030e2e50473d94aebbc4e0cbe2b36dcea1
Kathy Lin <katherine.m.lin@gmail.com>
2017-10-21 14:49:16 -0700

    First commit

---
+++ AGoodStart/AGoodStart/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  AGoodStart
+//
+//  Created by Kathy Lin on 10/21/17.
+//  Copyright © 2017 Kathy Lin. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
